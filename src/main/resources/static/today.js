966223ce950ff42fce7afa83f570b1c33c003b41
H-Iino2 <hiro110pg@gmail.com>
2022-10-01 15:42:07 +0900

    date test

--- src/main/resources/static/today.js
+++ src/main/resources/static/today.js
@@ -6,6 +6,8 @@ function today() {
     var mm = ("0"+(today.getMonth()+1));
     mm.slice(-2);
     var dd = (today.getDate());
+    console.log(mm);
+    console.log(dd);
     document.getElementById("date").value=yyyy+'-'+mm+'-'+dd;
 }
 //前月の月初と月末を初期値にいれる
